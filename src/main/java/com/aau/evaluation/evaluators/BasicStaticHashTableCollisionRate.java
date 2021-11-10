3737e969acd7d118a4f89b18ad9f74c45893f6ff
MrPekar98 <martin.pekar@outlook.dk>
2021-11-10 11:02:53 +0100

    Testing

---
+++ src/main/java/com/aau/evaluation/evaluators/BasicStaticHashTableCollisionRate.java
@@ -0,0 +1,46 @@
+package com.aau.evaluation.evaluators;
+
+import com.aau.evaluation.structs.BasicStaticHashTable;
+import org.apache.jena.graph.Triple;
+
+import java.util.Collection;
+
+public class BasicStaticHashTableCollisionRate implements Evaluatable
+{
+    private static final String UNIT = "%";
+    private double prog = 0;
+    private String evaluationName;
+    private Collection<Triple> triples;
+    private BasicStaticHashTable hashTable;
+
+    public BasicStaticHashTableCollisionRate(Collection<Triple> triples, String evalName)
+    {
+        this.triples = triples;
+        this.evaluationName = evalName;
+    }
+
+    @Override
+    public String title()
+    {
+        return this.evaluationName;
+    }
+
+    @Override
+    public String unit()
+    {
+        return UNIT;
+    }
+
+    @Override
+    public double progress()
+    {
+        return this.prog;
+    }
+
+    @Override
+    public double eval()
+    {
+        this.hashTable = new BasicStaticHashTable(this.triples.toArray());
+        return (1 - (this.hashTable.size() % this.triples.size())) * 100;
+    }
+}
